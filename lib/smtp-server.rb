236db3b48e284d8692177f94b21caa833e2c0f82
Tayden Miller <tayden007@hotmail.com>
2024-11-06 14:49:30 -0700

    ok

--- lib/smtp-server.rb
+++ lib/smtp-server.rb
@@ -58,13 +58,12 @@ module SmtpServer
     end
 
     def start
-      @ports.each do |port|
+      @logger.info("SMTP Servers started on ports: #{ @ports.join(', ') }")
+      @ports.each do | port |
         server = Server.new(port: port)
         @servers << server
         Thread.new { server.start }
       end
-
-      @logger.info("SMTP Servers started on ports: #{ @ports.join(', ') }")
 
       loop do
         @servers.each do |server|
